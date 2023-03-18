ed87d4366c6b87865d724ac8bb1330ec4ce2ad66
duwles <broxindev@gmail.com>
2023-03-18 19:12:27 +0100

    commit

---
+++ src/firmware.h
@@ -0,0 +1,8 @@
+#ifndef FIRMWARE_H__
+#define FIRMWARE_H__
+#include <Arduino.h>
+
+
+
+
+#endif // !FIRMWARE_H__
--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,85 @@
 #include <PID_v1.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include "definitions.h"
-#include "config.h"
 
-#include "HeatT12.h"
-#include "duwles.h"
+#include "config.h"
+#include "definitions.h"
+#include "hardware.h"
+
+#include <HAL.hpp>
+
+double g_Output = 125.0;
 
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
 
 
+//Przerwanie wywolywane 100 razy na sekunde
+// ISR(TIMER1_COMPA_vect) {}
+
+
+//ISR(INT0_vect) {	
+void tiltsw_vect() {
+  // Kod obsługi przerwania 
+  //			@INT0, 		- D2
+  	Serial.println("vector-swVibration");
+}
+
+//ISR(INT1_vect) {
+void encoder_vect() {
+  // Kod obsługi przerwania 
+  //			@INT1, 		- D3
+  Serial.println("Encoder call.");
+}
+
+
+// Przerwanie od przetwornika ADC 
+// ISR(ADC_vect) {}
+
+
 void setup() {
+  // IO Pin mapping
+	for(uint8_t pin_uid : DigitalIO_Pullup) {
+		pinMode(pin_uid, INPUT_PULLUP);
+	}
+
+	for(uint8_t pin_uid : DigitalIO_Output) {
+		pinMode(pin_uid, OUTPUT);
+	}
+
+  // Init Timer1	
 	Timer1.initialize(1000000);
+	
 
-	Serial.begin(9600);
+  // Start I2C serial
+	Wire.begin();	
 
+  // Start serial communication port
+	Serial.begin(19200);
+	Serial.println(" Initializing...");
+
+  // Start Lcd1602 Display layer
 	lcd.backlight();
 	lcd.init();
 	lcd.println(" Initializing...");
 
-  // Start Iterupt engine	
+
+  // Włączenie przerwań INT0, INT1 dla pinów (pin D2, D3)
+	attachInterrupt(digitalPinToInterrupt(TILTSW_PIN), tiltsw_vect, 	CHANGE);	
+	attachInterrupt(digitalPinToInterrupt(ENCCLK_PIN), encoder_vect, CHANGE);
+
+  // Uruchomienie przerwań
 	sei();	
+
 } // setup()
 
 
 
 void loop() {
 
+	cli();
+	PORTD |= (1 << PD2);
+	delay(1600);
+
+	PORTD &= ~(1 << PD2);
+	delay(2000);
 } // loop()
